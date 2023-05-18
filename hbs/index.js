d1f24b04eae5f7654fb84fa26dc1b379116a1772
camiloperez98 <jcperez6249@misena.edu.co>
2023-05-17 21:32:03 -0500

    air

---
+++ hbs/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const hbs = require('hbs')
+
+const port = 8085
+
+app.use(express.static('public'))
+
+app.set('views', path.join(__dirname + '/public/views'))
+app.set('view engine', 'hbs')
+
+hbs.registerPartials(__dirname + '/public/views/partials');
+
+app.get('/', (req, res) =>{
+    res.render('disponibilidad',{
+        titulo:'Disponibilidad',
+        nombre:''
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Escuchando por el puerto ${port}`)
+})
